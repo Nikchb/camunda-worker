30652e4430c863f2bbae45eaf264542beff237e7
nikitachubko <nikitachubko@gmail.com>
2025-02-27 21:57:44 +0100

    BPMNError default errorMessage added

--- package/dist/modules/BPMNError.js
+++ package/dist/modules/BPMNError.js
@@ -1,6 +1,6 @@
 export default class BPMNError extends Error {
     constructor(errorCode, variables = {}, errorMessage) {
-        super(errorMessage !== null && errorMessage !== void 0 ? errorMessage : "");
+        super(errorMessage !== null && errorMessage !== void 0 ? errorMessage : "BPMN Error");
         this.errorCode = errorCode;
         this.variables = variables;
     }
--- package/modules/BPMNError.ts
+++ package/modules/BPMNError.ts
@@ -3,7 +3,7 @@ export default class BPMNError extends Error {
   public variables: Record<string, any>;
 
   constructor(errorCode: string, variables: Record<string, any> = {}, errorMessage?: string) {
-    super(errorMessage ?? "");
+    super(errorMessage ?? "BPMN Error");
     this.errorCode = errorCode;
     this.variables = variables;
   }
